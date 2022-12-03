7534d5b7bfb4ffe904c35b6259c0e1029ba8df85
ismaelahmad6333 <106573961+ismaelahmad6333@users.noreply.github.com>
2022-12-03 06:42:30 +0000

    initial commit

---
+++ src/components/Navbar/Navbar.js
@@ -0,0 +1,19 @@
+const Navbar = () => {
+  return (
+    <nav className="container nav">
+      <div className="nav-center">
+        <div className="nav-header">
+          <h2>This interior</h2>
+          <ul>
+            <li>Home</li>
+            <li>Collection</li>
+            <li>About</li>
+            <li>Contact</li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+  )
+}
+
+export default Navbar
