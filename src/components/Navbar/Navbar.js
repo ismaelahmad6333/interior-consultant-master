ae1f6f250f3dd998817200a9cd3e75de3958ba51
ismaelahmad6333 <106573961+ismaelahmad6333@users.noreply.github.com>
2022-12-03 08:47:40 +0000

    fix cta button

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,15 +1,21 @@
+import { useState } from 'react'
+
 const Navbar = () => {
+  const [open, setOpen] = useState(false)
   return (
     <nav className="container nav">
       <div className="nav-center">
         <div className="nav-header">
           <h2>This interior</h2>
-          <ul>
+          <ul className={open ? 'links show-links' : 'links'}>
             <li>Home</li>
             <li>Collection</li>
             <li>About</li>
             <li>Contact</li>
           </ul>
+          <button className="btn" onClick={() => setOpen(!open)}>
+            |||||
+          </button>
         </div>
       </div>
     </nav>
